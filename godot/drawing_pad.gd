a5fb72d6579168c10ca78e0e942d4fc53253cf66
amm <aksel.martin.muru@ut.ee>
2024-12-08 13:54:09 +0200

    imporived drawing pad size

--- godot/drawing_pad.gd
+++ godot/drawing_pad.gd
@@ -8,6 +8,8 @@ var _current_stroke: PackedVector2Array
 
 func _process(delta: float) -> void:
 	var current_pos := get_local_mouse_position().clamp(Vector2.ZERO, size)
+	if not get_rect().has_point(current_pos):
+		return
 	
 	if Input.is_action_pressed("mouse_click"):
 		_current_stroke.append(current_pos)
@@ -28,7 +30,11 @@ func _draw() -> void:
 
 func to_object() -> Drawing:
 	var d := Drawing.new()
-	d.strokes = _strokes
+	d.strokes = []
+	for stroke in _strokes:
+		d.strokes.append(PackedVector2Array())
+		for point in stroke:
+			d.strokes.back().append(point / 2)
 	return d
 
 
